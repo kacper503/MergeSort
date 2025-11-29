a4df83ebadc2eab03993a15890ac6cb0f1602ed9
kacper503 <kt16127@student.ans-ns.edu.pl>
2025-11-29 11:31:40 +0100

    main, float/double

--- main.cpp
+++ main.cpp
@@ -4,23 +4,46 @@
 using namespace std;
 
 int main() {
-    // Przykładowy wektor do posortowania
+    // Testowanie na INT
     vector<int> data = { 9, 1, 5, 3, 7, 2, 8, 6, 4 };
+    MergeSort<int> sorterInt;
 
-    // Obiekt klasy MergeSort
-    MergeSort<int> sorter;
-
-    cout << "Przed sortowaniem: ";
-    for (auto& x : data)
-        cout << x << " ";
+    cout << "INT - Przed sortowaniem: ";
+    for (auto& x : data) cout << x << " ";
     cout << endl;
 
-    // Sortowanie danych
-    sorter.sort(data);
+    sorterInt.sort(data);
 
-    cout << "Po sortowaniu:    ";
-    for (auto& x : data)
-        cout << x << " ";
+    cout << "INT - Po sortowaniu:    ";
+    for (auto& x : data) cout << x << " ";
+    cout << endl << endl;
+
+    // Testowanie na Float
+    vector<float> dataF = { 3.5f, 1.1f, 2.8f, 0.4f, 9.9f };
+    MergeSort<float> sorterFloat;
+
+    cout << "FLOAT - Przed sortowaniem: ";
+    for (auto& x : dataF) cout << x << " ";
+    cout << endl;
+
+    sorterFloat.sort(dataF);
+
+    cout << "FLOAT - Po sortowaniu:    ";
+    for (auto& x : dataF) cout << x << " ";
+    cout << endl << endl;
+
+    // Testowanie na Double
+    vector<double> dataD = { 10.01, 7.7, 3.1415, -2.5, 8.8 };
+    MergeSort<double> sorterDouble;
+
+    cout << "DOUBLE - Przed sortowaniem: ";
+    for (auto& x : dataD) cout << x << " ";
+    cout << endl;
+
+    sorterDouble.sort(dataD);
+
+    cout << "DOUBLE - Po sortowaniu:    ";
+    for (auto& x : dataD) cout << x << " ";
     cout << endl;
 
     return 0;
